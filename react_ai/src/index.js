9ced571a497a868ed3ef3d99b44c648ec36fd79d
Waruna <warunahello@gmail.com>
2023-08-09 01:47:55 +0530

    dashboard added

--- react_ai/src/index.js
+++ react_ai/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
