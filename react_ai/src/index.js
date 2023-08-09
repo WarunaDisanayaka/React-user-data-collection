af9b5653b47ae1c3a0617bd864d1074264f1d969
Waruna <warunahello@gmail.com>
2023-08-09 08:43:01 +0530

    Added user section adding part

--- react_ai/src/index.js
+++ react_ai/src/index.js
@@ -1,17 +1,17 @@
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+
 
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
